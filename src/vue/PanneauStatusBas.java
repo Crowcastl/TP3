613d00ae830cee0e2d5c19c7fe2ce7298a37d627
Jonathan <jonathan.savard.3@ens.etsmtl.ca>
2022-04-17 14:39:25 -0400

    Grandeur bas régler

--- src/vue/PanneauStatusBas.java
+++ src/vue/PanneauStatusBas.java
@@ -1,9 +1,12 @@
 package vue;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.util.LinkedList;
 import java.util.Queue;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -24,16 +27,18 @@ public class PanneauStatusBas extends JPanel {
 	private JScrollPane jScroll;
 	
 	public PanneauStatusBas(Dimension dimention) {
-		this.dimention = new Dimension(dimention);
-		this.dimention.setSize(dimention.width,dimention.height);
+		this.setLayout(new GridLayout(1,0));
+		this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		
-		jTexte = new JTextArea(dimention.width/30, dimention.height/19);
-		jTexte.setSize(this.dimention);
+		this.dimention = new Dimension(dimention);
+
+		
+		jTexte = new JTextArea();
 		jTexte.setCaretPosition(jTexte.getDocument().getLength());// Pour mettre la scroll bar toujours en bas
 		jTexte.setEditable(false);
 		
 		jScroll = new JScrollPane(jTexte);
-		jScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+//		jScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);// Pour faire défiler le scroll automatiquement, fct pas
 		this.add(jScroll);		
 	}
 	
